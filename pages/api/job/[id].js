dc47489ddd0bb725fdc9e3ae45daff6b1b9e5b47
developereleven11-create <developer.eleven11@gmail.com>
2025-10-03 11:08:52 +0530

    Update [id].js

--- pages/api/job/[id].js
+++ pages/api/job/[id].js
@@ -1,8 +1,20 @@
-import { sql } from "@vercel/postgres";
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 export default async function handler(req, res) {
   const { id } = req.query;
-  const { rows: job } = await sql`SELECT * FROM jobs WHERE id=${id}`;
-  const { rows: results } = await sql`SELECT * FROM job_results WHERE job_id=${id}`;
-  res.json({ job: job[0], results });
+  const client = await pool.connect();
+
+  try {
+    const job = await client.query("SELECT * FROM jobs WHERE id = $1", [id]);
+    const results = await client.query("SELECT * FROM job_results WHERE job_id = $1", [id]);
+    res.json({ job: job.rows[0], results: results.rows });
+  } finally {
+    client.release();
+  }
 }
